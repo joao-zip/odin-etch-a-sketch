6ee75bc5ef69c6285e4b2f08a32ff3231f43fc07
joao-zip <oliveira.joao@unifesp.br>
2023-06-09 09:48:14 -0300

    added: functions to buttons

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const ERASER = 1;
+const DEFAULT = 2;
+const RAINBOW = 3;
+
+let colorMode = DEFAULT;
+
 function makeGrid(size) {
     let grid = document.querySelector(".grid-container");
     
@@ -6,17 +12,49 @@ function makeGrid(size) {
 
     for(let i = 0; i < size * size; i++) {
         let gridElement = document.createElement("div");
-            
+        gridElement.addEventListener('mouseover', setColor);
         grid.appendChild(gridElement);
     }
-
+}
+ 
+function setColor(event) {
+    if(colorMode == DEFAULT) {
+        event.target.style.backgroundColor = document.querySelector('#color-selector').value;
+    } else if(colorMode == ERASER) {
+        event.target.style.backgroundColor = '#FFFFFF';
+    } else if(colorMode == RAINBOW) {
+        let color1 = Math.floor(Math.random() * 256);
+        let color2 = Math.floor(Math.random() * 256);
+        let color3 = Math.floor(Math.random() * 256);
+        event.target.style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`;
+    }
 }
 
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
+let eraser = document.getElementById('menu-eraser');
+eraser.addEventListener('click', () => {
+    colorMode = ERASER;
+})
+
+let colorSelector = document.getElementById('color-selector');
+colorSelector.addEventListener('click', () => {
+    colorMode = DEFAULT;
+})
+
+let rainbow = document.getElementById('menu-rainbow');
+rainbow.addEventListener('click', () => {
+    colorMode = RAINBOW;
+})
 
 let gridRange = document.getElementById("menu-range"); 
+document.querySelector(".size").innerHTML = `Size: ${gridRange.value} x ${gridRange.value}`;
+makeGrid(gridRange.value);
 gridRange.addEventListener('click', () => {
     let value = gridRange.value;
     makeGrid(value);
     document.querySelector(".size").innerHTML = `Size: ${value} x ${value}`;
 })
+
